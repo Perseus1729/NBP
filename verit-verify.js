5982a407505dab752b55f0e18046aa09a673799f
Hitesh Kumar Punna <62385692+Perseus1729@users.noreply.github.com>
2021-06-19 20:20:39 +0530

    Update verit-verify.js

--- verit-verify.js
+++ verit-verify.js
@@ -40,41 +40,44 @@ let ciphertext=localStorage.getItem("PrivKey");
 let passphrase=localStorage.getItem("password");
 let address2_privKey = CryptoJS.AES.decrypt(ciphertext, passphrase).toString(CryptoJS.enc.Utf8);
 const acc = maticweb3.eth.accounts.privateKeyToAccount(address2_privKey);
-console.log(acc);
+//console.log(acc);
 maticweb3.eth.accounts.wallet.add(acc);
 let signat = maticweb3.eth.accounts.sign(maticweb3.utils.keccak256(maticweb3.utils.encodePacked("Verit Platform Registration\n", address2)), owner_privKey);
 //console.log(signat);
 let sign  = signat.signature;
 var result =  await contract.methods.registerAddress("User", [], sign).send({from: address2, gas:100000 });
-console.log(result);
+//console.log(result);
 //Adding Attestation
 let platformHandle = "atharv";
 signat = maticweb3.eth.accounts.sign(maticweb3.utils.keccak256(maticweb3.utils.encodePacked("Verit Platform Attestation\n", platformHandle + "\n", address2)), owner_privKey);
 sign  = signat.signature;
 
 result = await contract.methods.addAttestation(["Twitter", "twitter.com", sign, "atharv"]).send({from: address2, gas:800000 });
-console.log(result);
+//console.log(result);
 //Adding record
 let platformIdentifier = "Twitter";
 let inputMessage = '0xcedf45072aa31710694cc8d8ff2f1697900077d7ebba2622d600771f1104f101';
 let bgpage = chrome.extension.getBackgroundPage();
 let word = bgpage.word.trim();
-console.log(word);
+//console.log(word);
 let index = 1;
 let dataIndexHash = '0xb10e2d527612073b26eecdfd717e6a320cf44b4afac2b0732d9fcbe2b7fa0cf6';
 let timeStamp = '123456';
 let platformSignature = "12";
 let platformMetadata = "12";
-while((word!="Sign")&&(word!="")){
-    inputMessage = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(word));
-//    dataIndexHash = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(index));
+inputMessage = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(word));
+let input_index="1406255228861648897";
+dataIndexHash = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(input_index));
+if((word!="Sign")&&(word!="")){
+    //    dataIndexHash = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(index));
     var new_signat = maticweb3.eth.accounts.sign(maticweb3.utils.keccak256(maticweb3.utils.encodePacked("Verit Trace Records\n", platformIdentifier + "\n", inputMessage, dataIndexHash, timeStamp)), address2_privKey);
 //    console.log(new_signat);
     sign = new_signat.signature;
-//    console.log(sign);
+    console.log(sign);
     let record = [dataIndexHash,sign,platformHandle,inputMessage,timeStamp,platformIdentifier,platformSignature,platformMetadata];
-//    console.log(record);
+    console.log(record);
     const init1 = async () =>{
+    console.log("Hello");
     const new_result = await record_contract.methods.addRecord(record).send({from: address2, gas:900000 });
     console.log(new_result);
     index = index + 1;
@@ -83,7 +86,32 @@ while((word!="Sign")&&(word!="")){
 //    localStorage.setItem("Localrecord", record_contract);     
     }    
     init1();
+
 }
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+    var index = document.getElementById('index').value;
+//    console.log(index);
+    var message = document.getElementById('message').value;
+//    console.log(message);
+    var n_platformIdentifier = document.getElementById('platform').value;
+//    console.log(n_platformIdentifier);
+    const init2 = async () =>{
+//        var output = await record_contract.methods.verifyRecord(dataIndexHash,inputMessage,platformIdentifier).call({from: address2, gas:900000 });
+//        console.log(output);
+        var n_dataIndexHash = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(index));
+        var n_inputMessage = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(message));
+        output = await record_contract.methods.verifyRecord(n_dataIndexHash,n_inputMessage,n_platformIdentifier).call({from: address2, gas:900000 });
+//        console.log(output);
+        if(output){
+            alert('Correct Message');
+        }
+        else{
+            alert('Wrong Message');
+        }
+    }
+    init2();
+});
 
 } 
 
