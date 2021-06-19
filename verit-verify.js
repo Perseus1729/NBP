538929eac9db64d6cb81e01cee3babfcd01d92a1
Hitesh Kumar Punna <62385692+Perseus1729@users.noreply.github.com>
2021-06-19 12:22:47 +0530

    Update verit-verify.js

--- verit-verify.js
+++ verit-verify.js
@@ -16,19 +16,19 @@ const init = async () =>{
     const VeritIdentityTable = require('./contracts/build/contracts/VeritIdentityTable.json');
     const maticweb3 = new Web3(new Web3.providers.HttpProvider('https://rpc-mumbai.maticvigil.com'));
     console.log(maticweb3);
-    const id= await maticweb3.eth.net.getId();
+//    const id= await maticweb3.eth.net.getId();
 //    console.log(id);
-    const deployedNetwork = VeritIdentityTable.networks[id];
+//    const deployedNetwork = VeritIdentityTable.networks[id];
 //    console.log(deployedNetwork);
     const contract = new maticweb3.eth.Contract(
         VeritIdentityTable.abi,
-        deployedNetwork.address
+        '0xD86A4b3021c4095397A9923e99F728943925CBe8'
     );
-//    console.log(contract);
-    const RecordsNetwork = VeritTraceRecords.networks[id];
+    console.log(contract);
+//    const RecordsNetwork = VeritTraceRecords.networks[id];
     const record_contract = new maticweb3.eth.Contract(
         VeritTraceRecords.abi,
-        RecordsNetwork.address
+        '0xA18d955d14B6F618156b358533D43C2E4e763789'
     );
 //    console.log(record_contract);
     //contract  Initiating Adding User
@@ -39,18 +39,20 @@ let address2 = localStorage.getItem("Address");
 let ciphertext=localStorage.getItem("PrivKey");
 let passphrase=localStorage.getItem("password");
 let address2_privKey = CryptoJS.AES.decrypt(ciphertext, passphrase).toString(CryptoJS.enc.Utf8);
+const acc = maticweb3.eth.accounts.privateKeyToAccount(address2_privKey);
+console.log(acc);
+maticweb3.eth.accounts.wallet.add(acc);
 let signat = maticweb3.eth.accounts.sign(maticweb3.utils.keccak256(maticweb3.utils.encodePacked("Verit Platform Registration\n", address2)), owner_privKey);
 //console.log(signat);
 let sign  = signat.signature;
-
-var result =  await contract.methods.registerAddress("User", [], sign).call();
+var result =  await contract.methods.registerAddress("User", [], sign).send({from: address2, gas:100000 });
 console.log(result);
 //Adding Attestation
 let platformHandle = "atharv";
 signat = maticweb3.eth.accounts.sign(maticweb3.utils.keccak256(maticweb3.utils.encodePacked("Verit Platform Attestation\n", platformHandle + "\n", address2)), owner_privKey);
 sign  = signat.signature;
 
-result = await contract.methods.addAttestation(["Twitter", "twitter.com", sign, "atharv"]).call();
+result = await contract.methods.addAttestation(["Twitter", "twitter.com", sign, "atharv"]).send({from: address2, gas:800000 });
 console.log(result);
 //Adding record
 let platformIdentifier = "Twitter";
@@ -65,7 +67,7 @@ let platformSignature = "12";
 let platformMetadata = "12";
 while((word!="Sign")&&(word!="")){
     inputMessage = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(word));
-    dataIndexHash = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(index));
+//    dataIndexHash = maticweb3.utils.keccak256(maticweb3.utils.encodePacked(index));
     var new_signat = maticweb3.eth.accounts.sign(maticweb3.utils.keccak256(maticweb3.utils.encodePacked("Verit Trace Records\n", platformIdentifier + "\n", inputMessage, dataIndexHash, timeStamp)), address2_privKey);
 //    console.log(new_signat);
     sign = new_signat.signature;
@@ -73,7 +75,7 @@ while((word!="Sign")&&(word!="")){
     let record = [dataIndexHash,sign,platformHandle,inputMessage,timeStamp,platformIdentifier,platformSignature,platformMetadata];
 //    console.log(record);
     const init1 = async () =>{
-    const new_result = await record_contract.methods.addRecord(record).call({gas: 20000000});
+    const new_result = await record_contract.methods.addRecord(record).send({from: address2, gas:900000 });
     console.log(new_result);
     index = index + 1;
     word="Sign";
